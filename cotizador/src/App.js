7a1310f138c32c211d6846c88e4874b4f408af5a
luis gimenez <gimenezluis001@gmail.com>
2020-07-23 16:50:43 -0300

    primer commit

---
+++ cotizador/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
+
+import styled from '@emotion/styled';
+
+const Contendor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
+
+
+function App() {
+
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos:{
+      marca: '',
+      plan:'',
+      year:''
+    }
+  });
+
+  const [ cargando, guardarCargando ] = useState(false);
+  const { cotizacion, datos } = resumen;
+
+  return (
+  
+    <Contendor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario 
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+        {cargando ? <Spinner /> : null }
+        
+        <Resumen 
+          datos={datos}
+        />
+        {!cargando 
+          ? 
+            <Resultado 
+            cotizacion={cotizacion}
+            />
+          : null
+        }
+        
+      </ContenedorFormulario>
+    </Contendor>
+      
+    
+  );
+}
+
+
+export default App;
